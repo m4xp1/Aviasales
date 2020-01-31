b58ff1c1bb408930cdb7e0200d0fcd46a44f29cc
m4xp1 <dm@xcorp.one>
2020-01-31 21:15:41 +0300

    Integrate google map

--- app/src/main/java/one/xcorp/aviasales/screen/ticket/search/TicketSearchActivity.kt
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/search/TicketSearchActivity.kt
@@ -4,13 +4,33 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import one.xcorp.aviasales.R
 
 class TicketSearchActivity : AppCompatActivity() {
 
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_search)
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(::onMapReady)
+    }
+
+    private fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     companion object {
