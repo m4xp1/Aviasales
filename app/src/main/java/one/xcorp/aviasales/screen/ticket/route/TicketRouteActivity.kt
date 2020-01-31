7ebeacf6b185ef79cc98fdfdbc8f43ca91a006f7
m4xp1 <dm@xcorp.one>
2020-01-31 20:06:40 +0300

    Open TicketSearchActivity when start application

--- app/src/main/java/one/xcorp/aviasales/screen/ticket/route/TicketRouteActivity.kt
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/route/TicketRouteActivity.kt
@@ -3,11 +3,14 @@ package one.xcorp.aviasales.screen.ticket.route
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import one.xcorp.aviasales.R
+import one.xcorp.aviasales.screen.ticket.search.TicketSearchActivity
 
 class TicketRouteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_route)
+
+        startActivity(TicketSearchActivity.newIntent(this))
     }
 }
--- app/src/main/java/one/xcorp/aviasales/screen/ticket/search/TicketSearchActivity.kt
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/search/TicketSearchActivity.kt
@@ -1,5 +1,7 @@
 package one.xcorp.aviasales.screen.ticket.search
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import one.xcorp.aviasales.R
@@ -9,5 +11,12 @@ class TicketSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_search)
+    }
+
+    companion object {
+
+        fun newIntent(
+            context: Context
+        ) = Intent(context, TicketSearchActivity::class.java)
     }
 }
