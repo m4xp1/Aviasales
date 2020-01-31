832586f8b1054cf50ec72e6eb394b1c000739796
m4xp1 <dm@xcorp.one>
2020-01-31 19:59:02 +0300

    Added TicketSearchActivity

---
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/search/TicketSearchActivity.kt
@@ -0,0 +1,13 @@
+package one.xcorp.aviasales.screen.ticket.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import one.xcorp.aviasales.R
+
+class TicketSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket_search)
+    }
+}
