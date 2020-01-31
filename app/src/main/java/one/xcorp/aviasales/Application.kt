7da2309d3829b80849608a151411f7719bfb93f0
m4xp1 <dm@xcorp.one>
2020-01-31 12:50:47 +0300

    initial project

---
+++ app/src/main/java/one/xcorp/aviasales/Application.kt
@@ -0,0 +1,8 @@
+package one.xcorp.aviasales
+
+class Application : android.app.Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/route/RouteActivity.kt
@@ -0,0 +1,13 @@
+package one.xcorp.aviasales.screen.ticket.route
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import one.xcorp.aviasales.R
+
+class RouteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_route)
+    }
+}
