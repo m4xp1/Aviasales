be16d0c19152a83ba82d1e9b4e720265503b7639
m4xp1 <dm@xcorp.one>
2020-02-01 17:37:07 +0300

    Show airport markers on TicketSearchActivity

--- app/src/main/java/one/xcorp/aviasales/screen/ticket/route/TicketRouteActivity.kt
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/route/TicketRouteActivity.kt
@@ -2,7 +2,9 @@ package one.xcorp.aviasales.screen.ticket.route
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.model.LatLng
 import one.xcorp.aviasales.R
+import one.xcorp.aviasales.screen.ticket.route.model.AirportModel
 import one.xcorp.aviasales.screen.ticket.search.TicketSearchActivity
 
 class TicketRouteActivity : AppCompatActivity() {
@@ -11,6 +13,9 @@ class TicketRouteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_route)
 
-        startActivity(TicketSearchActivity.newIntent(this))
+        val departureAirport = AirportModel("St. Petersburg", "LED", LatLng(59.942652, 30.311771))
+        val destinationAirport = AirportModel("Moscow", "MOW", LatLng(55.755788, 37.627144))
+
+        startActivity(TicketSearchActivity.newIntent(this, departureAirport, destinationAirport))
     }
 }
--- app/src/main/java/one/xcorp/aviasales/screen/ticket/search/TicketSearchActivity.kt
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/search/TicketSearchActivity.kt
@@ -4,20 +4,29 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.CameraUpdateFactory.newLatLngBounds
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLngBounds
 import one.xcorp.aviasales.R
+import one.xcorp.aviasales.screen.ticket.route.model.AirportModel
+import one.xcorp.aviasales.screen.ticket.search.marker.AirportMarkerFactory
 
 class TicketSearchActivity : AppCompatActivity() {
+
+    private val airportMarkerFactory by lazy { AirportMarkerFactory(layoutInflater) }
+
+    private lateinit var departureAirport: AirportModel
+    private lateinit var destinationAirport: AirportModel
 
     private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_search)
+
+        departureAirport = requireNotNull(intent.getParcelableExtra(DEPARTURE_AIRPORT_KEY))
+        destinationAirport = requireNotNull(intent.getParcelableExtra(DESTINATION_AIRPORT_KEY))
 
         val mapFragment =
             supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
@@ -26,17 +35,42 @@ class TicketSearchActivity : AppCompatActivity() {
 
     private fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.uiSettings.isMapToolbarEnabled = false
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        addAirportMarker(departureAirport)
+        addAirportMarker(destinationAirport)
+
+        val latLngBounds = newLatLngBounds(
+            getInitialMapExtent(),
+            resources.displayMetrics.widthPixels,
+            resources.displayMetrics.heightPixels,
+            resources.getDimensionPixelSize(R.dimen.ticket_search_activity_map_extent_padding)
+        )
+
+        map.moveCamera(latLngBounds)
+    }
+
+    private fun getInitialMapExtent() = LatLngBounds.builder()
+        .include(departureAirport.location)
+        .include(destinationAirport.location)
+        .build()
+
+    private fun addAirportMarker(airport: AirportModel) = with(airport) {
+        map.addMarker(airportMarkerFactory.create(iata, location))
     }
 
     companion object {
 
+        private const val DEPARTURE_AIRPORT_KEY = "departure_airport"
+        private const val DESTINATION_AIRPORT_KEY = "destination_airport"
+
         fun newIntent(
-            context: Context
-        ) = Intent(context, TicketSearchActivity::class.java)
+            context: Context,
+            departureAirport: AirportModel,
+            destinationAirport: AirportModel
+        ) = Intent(context, TicketSearchActivity::class.java).apply {
+            putExtra(DEPARTURE_AIRPORT_KEY, departureAirport)
+            putExtra(DESTINATION_AIRPORT_KEY, destinationAirport)
+        }
     }
 }
