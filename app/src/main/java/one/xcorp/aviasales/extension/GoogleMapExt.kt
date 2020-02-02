b8cc0a72dbbe7c0336be5151a18dd7c86954e886
m4xp1 <dm@xcorp.one>
2020-02-03 00:05:36 +0300

    Added animate extension method for Marker

---
+++ app/src/main/java/one/xcorp/aviasales/extension/GoogleMapExt.kt
@@ -0,0 +1,37 @@
+package one.xcorp.aviasales.extension
+
+import android.animation.ValueAnimator
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.maps.android.SphericalUtil
+
+fun LatLng.interpolate(dest: LatLng, fraction: Float): LatLng =
+    SphericalUtil.interpolate(this, dest, fraction.toDouble())
+
+fun LatLng.bearingTo(dest: LatLng): Float = SphericalUtil.computeHeading(this, dest).toFloat()
+
+fun Marker.animate(dest: LatLng): ValueAnimator = animate(listOf(dest))
+
+fun Marker.animate(route: List<LatLng>): ValueAnimator {
+    require(route.isNotEmpty()) { "Cannot animate marker on empty route" }
+
+    val positions = arrayListOf(position, *route.toTypedArray())
+    val values = (0..route.size).toList().map { it.toFloat() }
+
+    val animator = ValueAnimator.ofFloat(*values.toFloatArray())
+    animator.addUpdateListener { animation ->
+        val value = animation.animatedValue as Float
+
+        val index = value.toInt().coerceIn(route.indices)
+        val fraction = value - index
+
+        if (animation.currentPlayTime >= animation.duration) {
+            position = positions[index].interpolate(route.last(), animation.animatedFraction)
+        } else {
+            position = positions[index].interpolate(positions[index + 1], fraction)
+            rotation = position.bearingTo(positions[index + 1])
+        }
+    }
+
+    return animator
+}
--- app/src/main/java/one/xcorp/aviasales/extension/LatLngExt.kt
+++
@@ -1,9 +0,0 @@
-package one.xcorp.aviasales.extension
-
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.SphericalUtil
-
-fun LatLng.interpolate(dest: LatLng, fraction: Float): LatLng =
-    SphericalUtil.interpolate(this, dest, fraction.toDouble())
-
-fun LatLng.bearingTo(dest: LatLng): Float = SphericalUtil.computeHeading(this, dest).toFloat()
