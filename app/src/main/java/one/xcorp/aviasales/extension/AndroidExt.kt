eb19b58cdc27570a8cc29956cfc212fd3a52ef75
m4xp1 <dm@xcorp.one>
2020-02-03 10:37:37 +0300

    AndroidExt added updateCompoundDrawable TextView extension method

--- app/src/main/java/one/xcorp/aviasales/extension/AndroidExt.kt
+++ app/src/main/java/one/xcorp/aviasales/extension/AndroidExt.kt
@@ -2,7 +2,11 @@ package one.xcorp.aviasales.extension
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.widget.TextView
 import androidx.annotation.AnyRes
 import androidx.annotation.AttrRes
 import androidx.annotation.StringRes
@@ -27,4 +31,35 @@ fun TextInputLayout.showError(@StringRes resId: Int) {
 fun TextInputLayout.hideError() {
     isErrorEnabled = false
     error = null
+}
+
+private val CurrentDrawable = ColorDrawable(Color.TRANSPARENT)
+
+fun TextView.updateCompoundDrawable(
+    start: Drawable? = CurrentDrawable,
+    top: Drawable? = CurrentDrawable,
+    end: Drawable? = CurrentDrawable,
+    bottom: Drawable? = CurrentDrawable
+) {
+    val drawables = compoundDrawablesRelative
+
+    if (start !== CurrentDrawable) {
+        drawables[0] = start
+    }
+    if (top !== CurrentDrawable) {
+        drawables[1] = top
+    }
+    if (end !== CurrentDrawable) {
+        drawables[2] = end
+    }
+    if (bottom !== CurrentDrawable) {
+        drawables[3] = bottom
+    }
+
+    setCompoundDrawablesRelativeWithIntrinsicBounds(
+        drawables[0],
+        drawables[1],
+        drawables[2],
+        drawables[3]
+    )
 }
