47b09fb037520c0fa9e73ab8e410da18f4f96106
m4xp1 <dm@xcorp.one>
2020-02-03 10:01:49 +0300

    Added showError and hideError extension methods for TextInputLayout

--- app/src/main/java/one/xcorp/aviasales/extension/AndroidExt.kt
+++ app/src/main/java/one/xcorp/aviasales/extension/AndroidExt.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.view.View
 import androidx.annotation.AnyRes
 import androidx.annotation.AttrRes
+import androidx.annotation.StringRes
+import com.google.android.material.textfield.TextInputLayout
 
 val Activity.rootView: View
     get() = findViewById<View>(android.R.id.content)
@@ -16,3 +18,13 @@ fun Context.getThemeAttribute(@AttrRes resId: Int, @AnyRes default: Int): Int =
         recycle()
         id
     }
+
+fun TextInputLayout.showError(@StringRes resId: Int) {
+    isErrorEnabled = true
+    error = resources.getString(resId)
+}
+
+fun TextInputLayout.hideError() {
+    isErrorEnabled = false
+    error = null
+}
