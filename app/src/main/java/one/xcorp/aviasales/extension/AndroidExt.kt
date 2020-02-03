c76c15580c229d706f17854c76fa7def24d376a7
m4xp1 <dm@xcorp.one>
2020-02-03 03:30:07 +0300

    Added rootView field extension in Activity

--- app/src/main/java/one/xcorp/aviasales/extension/AndroidExt.kt
+++ app/src/main/java/one/xcorp/aviasales/extension/AndroidExt.kt
@@ -1,8 +1,13 @@
 package one.xcorp.aviasales.extension
 
+import android.app.Activity
 import android.content.Context
+import android.view.View
 import androidx.annotation.AnyRes
 import androidx.annotation.AttrRes
+
+val Activity.rootView: View
+    get() = findViewById<View>(android.R.id.content)
 
 @AnyRes
 fun Context.getThemeAttribute(@AttrRes resId: Int, @AnyRes default: Int): Int =
