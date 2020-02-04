9e1f07dc5fbe14f52b3b2bb5adb82312aea3e6bb
m4xp1 <dm@xcorp.one>
2020-02-04 08:27:33 +0300

    Added ApplicationComponent

---
+++ app/src/main/java/one/xcorp/aviasales/dagger/ApplicationComponent.kt
@@ -0,0 +1,32 @@
+package one.xcorp.aviasales.dagger
+
+import android.app.Application
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import one.xcorp.aviasales.dagger.module.ApplicationModule
+import one.xcorp.aviasales.data.dagger.DataComponent
+import one.xcorp.didy.scope.ApplicationScope
+
+@ApplicationScope
+@Component(
+    dependencies = [
+        DataComponent::class
+    ],
+    modules = [
+        ApplicationModule::class
+    ]
+)
+interface ApplicationComponent {
+
+    val context: Context
+
+    @Component.Factory
+    interface Factory {
+
+        fun createComponent(
+            @BindsInstance application: Application,
+            dataComponent: DataComponent
+        ): ApplicationComponent
+    }
+}
