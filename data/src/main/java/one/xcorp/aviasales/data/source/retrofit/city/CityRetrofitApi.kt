82a3cc448e30a5b1100abb24da031081e17135da
m4xp1 <dm@xcorp.one>
2020-02-04 04:33:42 +0300

    Added CityRetrofitApi

---
+++ data/src/main/java/one/xcorp/aviasales/data/source/retrofit/city/CityRetrofitApi.kt
@@ -0,0 +1,20 @@
+package one.xcorp.aviasales.data.source.retrofit.city
+
+import io.reactivex.Single
+import one.xcorp.aviasales.data.source.retrofit.city.response.AutocompleteResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+internal interface CityRetrofitApi {
+
+    @GET(METHOD_AUTOCOMPLETE)
+    fun autocomplete(
+        @Query("term") term: Int,
+        @Query("lang") lang: Int
+    ): Single<AutocompleteResponse>
+
+    companion object {
+
+        private const val METHOD_AUTOCOMPLETE = "autocomplete"
+    }
+}
