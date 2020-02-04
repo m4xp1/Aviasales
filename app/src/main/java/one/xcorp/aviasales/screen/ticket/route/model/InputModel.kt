36d741b60af8345b740cd11ee358f0a1dde1a5d0
m4xp1 <dm@xcorp.one>
2020-02-04 18:55:03 +0300

    Added default values for fields into InputModel

--- app/src/main/java/one/xcorp/aviasales/screen/ticket/route/model/InputModel.kt
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/route/model/InputModel.kt
@@ -1,10 +1,11 @@
 package one.xcorp.aviasales.screen.ticket.route.model
 
 import one.xcorp.mvvm.model.InputState
+import one.xcorp.mvvm.model.InputState.NotEntered
 
 data class InputModel(
-    val departure: InputState<CityModel>,
-    val destination: InputState<CityModel>
+    val departure: InputState<CityModel> = NotEntered,
+    val destination: InputState<CityModel> = NotEntered
 ) {
 
     object NotSelected : InputState.Error()
