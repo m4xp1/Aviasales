edb8af47c207c2743e5904a9775b24ff3bbc874c
m4xp1 <dm@xcorp.one>
2020-02-04 11:42:49 +0300

    Added mapping from LocationEntity to LocationModel

--- app/src/main/java/one/xcorp/aviasales/screen/ticket/route/mapper/LocationModel.kt
+++ app/src/main/java/one/xcorp/aviasales/screen/ticket/route/mapper/LocationModel.kt
@@ -1,8 +1,13 @@
 package one.xcorp.aviasales.screen.ticket.route.mapper
 
 import com.google.android.gms.maps.model.LatLng
+import one.xcorp.aviasales.domain.entity.LocationEntity
 import one.xcorp.aviasales.screen.ticket.route.model.LocationModel
 
 fun LocationModel.toLatLng() = LatLng(latitude, longitude)
 
 fun List<LocationModel>.toLatLng() = map { it.toLatLng() }
+
+fun LocationEntity.toLocationModel() = LocationModel(latitude, longitude)
+
+fun List<LocationEntity>.toLocationModel() = map { it.toLocationModel() }
