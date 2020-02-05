c7583c6e3c34351f039d77e7ac9fe3866038042e
m4xp1 <dm@xcorp.one>
2020-02-05 14:29:20 +0300

    Added FragmentActivity.obtainViewModel without body

--- app/src/main/java/one/xcorp/mvvm/ActivityExt.kt
+++ app/src/main/java/one/xcorp/mvvm/ActivityExt.kt
@@ -6,6 +6,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.Factory
 
 inline fun <reified T : ViewModel> FragmentActivity.obtainViewModel(
+    factory: Factory
+) = ViewModelProvider(this, factory)[T::class.java]
+
+inline fun <reified T : ViewModel> FragmentActivity.obtainViewModel(
     factory: Factory,
     body: T.() -> Unit
 ) = ViewModelProvider(this, factory)[T::class.java].apply(body)
