e8cc42aac482697929b8857a95d8424b56548ebd
m4xp1 <dm@xcorp.one>
2020-02-11 10:36:08 +0300

    Attempt to set uniform speed (revert)

--- app/src/main/java/one/xcorp/aviasales/extension/map/GoogleMapExt.kt
+++ app/src/main/java/one/xcorp/aviasales/extension/map/GoogleMapExt.kt
@@ -23,8 +23,6 @@ fun Rect.toProjection(projection: Projection): LatLngBounds {
 fun LatLngBounds.contains(bounds: LatLngBounds): Boolean =
     contains(bounds.southwest) && contains(bounds.northeast)
 
-fun LatLng.distanceTo(dest: LatLng): Double = SphericalUtil.computeDistanceBetween(this, dest)
-
 fun LatLng.bearingTo(dest: LatLng): Float = SphericalUtil.computeHeading(this, dest).toFloat()
 
 fun Marker.animate(dest: LatLng, interpolator: LatLngInterpolator = Spherical()): ValueAnimator =
@@ -36,41 +34,24 @@ fun Marker.animate(
 ): ValueAnimator {
     require(route.isNotEmpty()) { "Cannot animate marker on empty route" }
 
-    val points = arrayListOf(position, *route.toTypedArray())
+    val positions = arrayListOf(position, *route.toTypedArray())
+    val values = (0..route.size).toList().map { it.toFloat() }
 
-    val distances = mutableListOf(0.0)
-    for (i in 0 until points.lastIndex) {
-        val distanceToNextPoint = points[i].distanceTo(points[i + 1])
-        distances.add(distances.last() + distanceToNextPoint)
-    }
-    val values = distances
-        .map { (it / distances.last()).toFloat() }
-        .toFloatArray()
-
-    val animator = ValueAnimator.ofFloat(*values)
+    val animator = ValueAnimator.ofFloat(*values.toFloatArray())
     animator.addUpdateListener { animation ->
         val value = animation.animatedValue as Float
-        val index = values.binarySearch(value)
 
-        val from: Int
-        val to: Int
-        if (index < 0) {
-            from = -(index + 2)
-            to = -(index + 1)
-        } else {
-            from = index
-            to = index + 1
-        }
+        val index = value.toInt().coerceIn(route.indices)
+        val fraction = value - index
 
-        if (animation.currentPlayTime >= animation.duration || value == 1f) {
+        if (animation.currentPlayTime >= animation.duration) {
             position = interpolator.interpolate(
-                points[from], route.last(), animation.animatedFraction
+                positions[index], route.last(), animation.animatedFraction
             )
         } else {
-            val fraction = (value - values[from]) / (values[to] - values[from])
-            position = interpolator.interpolate(points[from], points[to], fraction)
+            position = interpolator.interpolate(positions[index], positions[index + 1], fraction)
             if (fraction != 1f) { // keep latest bearing
-                rotation = position.bearingTo(points[to])
+                rotation = position.bearingTo(positions[index + 1])
             }
         }
     }
